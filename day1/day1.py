c0d732f6b6c2aa2b832d65342c7c94426daf6a6b
ejy921 <ejy921@gmail.com>
2025-12-02 16:39:29 -0600

    day 1 pt 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,7 @@
 filename = "day1/input.txt"
 
 curr = 50
+bound = 100
 count_zero = 0
 
 with open(filename, "r") as f:
@@ -9,16 +10,28 @@ with open(filename, "r") as f:
         line = line.strip()
         if not line:
             continue
+    
+        dir = line[0]
+        moves = int(line[1:])
 
-        if line[0] == "L":
-            curr -= int(line[1:])
-        elif line[0] == "R":
-            curr += int(line[1:])
+        rotations = moves // bound
+        count_zero += rotations
+        
+        rem = moves % bound
+        if dir == "L":
+            if (curr - rem) < 0 and curr != 0:
+                count_zero += 1
+            curr -= rem
+        elif dir == "R":
+            if (curr + rem) > bound:
+                count_zero += 1
+            curr += rem
         else:
             raise ValueError("Invalid input")
 
-        curr %= 100
+        curr %= bound
         if curr == 0:
             count_zero += 1
+
 
     print(count_zero)
